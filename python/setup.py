330cb98648a449d0234a722d1af21c2782ffa110
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2014-03-04 22:30:39 +0000

    add setup script

---
+++ python/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='gridcells',
+      version='0.1',
+      description='Grid cells package',
+      author='Lukas Solanka',
+      author_email='lsolanka@gmail.com',
+      packages=['gridcells'])
