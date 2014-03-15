5ec0aa340930f1686619928257282dc21ce0fc4b
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2014-03-15 12:06:21 +0000

    add a simple armadillo version that computes a Gaussian

---
+++ gridcells/tests/profiling/gaussian_arma.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <armadillo>
+
+using namespace std;
+using namespace arma;
+
+const int NITER = 2395;
+const int SZ = 60000;
+const double sigma = 1.3;
+
+
+int main(int argc, char** argv)
+{
+
+
+    vec d2 = randu(SZ);
+    vec res(SZ);
+
+    for (int it = 0; it < NITER; it++) {
+        res = arma::exp(-d2 / 2.0 / (sigma*sigma));
+        //cout << it << endl;
+    }
+
+    
+    cout << "res[0]: " << res(0) << endl;
+
+    return 0;
+
+}
