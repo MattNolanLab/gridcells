16dd657e13718695e4700f3db8fab792166af445
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2014-03-17 21:17:44 +0000

    add namespace grids::
    All the grid cell specific c++ code is now in this namespace

--- src/fields.cpp
+++ src/fields.cpp
@@ -1,9 +1,11 @@
-
 #include "fields.hpp"
 
-using namespace arma;
 
-vec extractSpikePos(const vec& spikePosIdx, const vec& posData, double dt)
+namespace grids
+{
+
+arma::vec
+extractSpikePos(const arma::vec& spikePosIdx, const arma::vec& posData, double dt)
 {
     arma::vec res(spikePosIdx.n_elem);
     for (int i = 0; i < spikePosIdx.n_elem; i++) {
@@ -13,33 +15,34 @@ vec extractSpikePos(const vec& spikePosIdx, const vec& posData, double dt)
 }
 
 
-mat
-SNSpatialRate2D(const vec& spikeTimes, const vec& pos_x, const vec& pos_y, 
-        double dt, double arenaDiam, double h)
+arma::mat
+SNSpatialRate2D(const arma::vec& spikeTimes, const arma::vec& pos_x, const
+        arma::vec& pos_y, double dt, double arenaDiam, double h)
 {
     double precision = arenaDiam / h;
-    vec xedges = arma::linspace(-arenaDiam/2, arenaDiam/2, precision+1);
-    vec yedges = arma::linspace(-arenaDiam/2, arenaDiam/2, precision+1);
+    arma::vec xedges = arma::linspace(-arenaDiam/2, arenaDiam/2, precision+1);
+    arma::vec yedges = arma::linspace(-arenaDiam/2, arenaDiam/2, precision+1);
 
-    mat rateMap = arma::zeros(xedges.n_elem, yedges.n_elem);
-    vec spikePosIdx = spikeTimes / dt;
-    vec neuronPos_x = extractSpikePos(spikePosIdx, pos_x, dt);
-    vec neuronPos_y = extractSpikePos(spikePosIdx, pos_y, dt);
+    arma::mat rateMap = arma::zeros(xedges.n_elem, yedges.n_elem);
+    arma::vec spikePosIdx = spikeTimes / dt;
+    arma::vec neuronPos_x = extractSpikePos(spikePosIdx, pos_x, dt);
+    arma::vec neuronPos_y = extractSpikePos(spikePosIdx, pos_y, dt);
 
 
     int nIter = 0;
     for (int x_i = 0; x_i < xedges.n_elem; x_i++) {
         for (int y_i =0; y_i < yedges.n_elem; y_i++) {
-            cout << "nIter: " << nIter << endl;
+            //std::cout << "nIter: " << nIter << std::endl;
             double x = xedges(x_i);
             double y = yedges(y_i);
 
-            vec posDist2 = arma::square(pos_x - x) + arma::square(pos_y - y);
+            arma::vec posDist2 = arma::square(pos_x - x) +
+                                 arma::square(pos_y - y);
             bool isNearTrack = arma::accu(arma::sqrt(posDist2) <= h) > 0;
 
             if (isNearTrack) {
                 double normConst = arma::sum(arma::exp(-posDist2 / 2. / (h*h))) * dt;
-                vec neuronPosDist2 = arma::square(neuronPos_x - x) + 
+                arma::vec neuronPosDist2 = arma::square(neuronPos_x - x) + 
                                      arma::square(neuronPos_y - y);
                 double spikes = arma::sum(exp( -neuronPosDist2 / 2. / (h*h)));
                 rateMap(x_i, y_i) = spikes / normConst;
@@ -52,4 +55,4 @@ SNSpatialRate2D(const vec& spikeTimes, const vec& pos_x, const vec& pos_y,
     return rateMap.t();
 }
 
-
+} // namespace grids
--- src/include/fields.hpp
+++ src/include/fields.hpp
@@ -3,6 +3,9 @@
 
 #include <armadillo>
 
+namespace grids
+{
+
 arma::vec
 extractSpikePos(const arma::vec& spikePosIdx, const arma::vec& posData, double dt);
 
@@ -10,5 +13,6 @@ arma::mat
 SNSpatialRate2D(const arma::vec& spikeTimes, const arma::vec& pos_x, const
         arma::vec& pos_y, double dt, double arenaDiam, double h);
 
+} // namespace grids
 
 #endif // FIELDS_HPP
