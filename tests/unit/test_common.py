70a13cba436eb731d237fcdffd6defc1b49a0363
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2014-05-15 15:52:18 +0100

    tests are now run by pytest from the root of the project

--- tests/unit/test_common.py
+++ tests/unit/test_common.py
@@ -1,13 +1,11 @@
 from __future__ import absolute_import, division, print_function
 
-import unittest
-from unittest import TestCase, skip
 import numpy as np
 
 from gridcells.core import Position2D
 
 
-class TestPosition2D(TestCase):
+class TestPosition2D(object):
 
     def createPosition(self, nx, ny, dt):
         x = np.arange(nx)
@@ -16,9 +14,9 @@ class TestPosition2D(TestCase):
 
     def test_attributes(self):
         p = self.createPosition(10, 10, 0.02)
-        self.assertTrue(np.all(np.arange(10) == p.x))
-        self.assertTrue(np.all(np.arange(10)+10 == p.y))
-        self.assertEqual(.02, p.dt)
+        assert np.all(np.arange(10) == p.x)
+        assert np.all(np.arange(10)+10 == p.y)
+        assert .02 == p.dt
 
 
     #def test_equals(self):
--- tests/unit/test_fields.py
+++ tests/unit/test_fields.py
@@ -1,35 +1,13 @@
-'''
-.. currentmodule: gridcells.tests.test_fields
-
-The :mod:`~gridcells.test.test_fields` module defines a set of classes for unit
-testing of the firing field analysis code.  The module is currently based on
-the unittest module.
-
-A list of currently supported tests:
-
-.. autosummary::
-'''
-import unittest
+import pytest
 import numpy as np
 from gridcells.core import Position2D
 from gridcells.fields import spatialRateMap
 from gridcells.core import CircularArena, Pair2D
 import fields_ref_impl as refimp
 
-notImplMsg = "Not implemented"
 
-class TestRateFields(unittest.TestCase):
-    '''Test the correctness of firing field analysis.'''
-    rtol = 1e-3
-    arenaR = 90. # cm
-    sigma  = 3.  # cm
-
-    def setUp(self):
-        self.refData = None
-        self.refRateMap = None
-        self.refXedges = None
-        self.refYedges = None
-
+@pytest.fixture(scope='module')
+def reference_data():
     class InputData(object):
         def __init__(self, spikeTimes, pos_x, pos_y, pos_dt, arenaDiam, h):
             self.spikeTimes = spikeTimes
@@ -38,43 +16,44 @@ class TestRateFields(unittest.TestCase):
             self.pos_dt = pos_dt
             self.arenaDiam = arenaDiam
             self.h = h
+    dataDir = "tests/data"
+    ref_data = InputData(
+        spikeTimes = np.loadtxt("%s/spikeTimes.txt" % dataDir),
+        pos_x      = np.loadtxt("%s/pos_x.txt" % dataDir),
+        pos_y      = np.loadtxt("%s/pos_y.txt" % dataDir),
+        pos_dt     = 20,
+        arenaDiam  = 180.0,
+        h          = 3)
+    return ref_data
 
 
-    def loadRealDataSample(self):
-        if self.refData is None:
-            dataDir = "profiling/data"
-            d = self.InputData(
-                    spikeTimes = np.loadtxt("%s/spikeTimes.txt" % dataDir),
-                    pos_x      = np.loadtxt("%s/pos_x.txt" % dataDir),
-                    pos_y      = np.loadtxt("%s/pos_y.txt" % dataDir),
-                    pos_dt     = 20,
-                    arenaDiam  = 180.0,
-                    h          = 3)
-            refRateMap, refXedges, refYedges = \
-                refimp.SNSpatialRate2D(d.spikeTimes, d.pos_x, d.pos_y,
-                        d.pos_dt, d.arenaDiam, d.h)
-            self.refData = d
-            self.refRateMap = refRateMap
-            self.refXedges = refXedges
-            self.refYedges = refYedges
-        return self.refData, self.refRateMap, self.refXedges, self.refYedges
+@pytest.fixture(scope='module')
+def reference_spatial_map(reference_data):
+    d = reference_data
+    ref_rate_map, ref_xedges, ref_yedges = refimp.SNSpatialRate2D(
+            d.spikeTimes,
+            d.pos_x, d.pos_y, d.pos_dt,
+            d.arenaDiam,
+            d.h)
+    return d, ref_rate_map, ref_xedges, ref_yedges
 
 
-    def test_NaiveVersion(self):
-        # Reference
-        d, refRateMap, refXE, refYE = self.loadRealDataSample()
+class TestSpatialRateMap(object):
+    '''Test the correctness of firing field analysis.'''
+    rtol = 1e-3
+    arenaR = 90. # cm
+    sigma  = 3.  # cm
+
+    def test_NaiveVersion(self, reference_spatial_map):
+        d, ref_rate_map, ref_xedges, ref_yedges = reference_spatial_map
 
         # Tested code
         ar = CircularArena(self.arenaR, Pair2D(self.sigma, self.sigma))
         pos = Position2D(d.pos_x, d.pos_y, d.pos_dt)
         theirRateMap = spatialRateMap(d.spikeTimes, pos, ar, self.sigma)
         theirEdges = ar.getDiscretisation()
-        #print(np.max(np.abs(theirRateMap - refRateMap)))
-        np.testing.assert_allclose(theirRateMap, refRateMap, self.rtol)
-        np.testing.assert_allclose(theirEdges.x, refXE, self.rtol)
-        np.testing.assert_allclose(theirEdges.y, refYE, self.rtol)
+        #print(np.max(np.abs(theirRateMap - ref_rate_map)))
+        np.testing.assert_allclose(theirRateMap, ref_rate_map, self.rtol)
+        np.testing.assert_allclose(theirEdges.x, ref_xedges, self.rtol)
+        np.testing.assert_allclose(theirEdges.y, ref_yedges, self.rtol)
 
-
-    @unittest.skip(notImplMsg)
-    def test_OptimisedVersion(self):
-        pass
