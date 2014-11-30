edf97ccd3f2bfe21a82a756b6d0fa022808f46e8
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2014-11-30 20:08:09 +0000

    version number format is x.x.x

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ all_extensions = [
 
 setup(
     name='gridcells',
-    version='0.1',
+    version='0.1.0',
     description='Package for grid cell analysis and simulation.',
     author='Lukas Solanka',
     author_email='lsolanka@gmail.com',
