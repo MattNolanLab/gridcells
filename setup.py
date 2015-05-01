2bb75a6ffbe9290a0fe0a1c30eb2124a9ac6141a
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2015-05-01 11:11:55 +0100

    update Changelog

--- setup.py
+++ setup.py
@@ -116,7 +116,7 @@ all_extensions = [
 
 setup(
     name='gridcells',
-    version='0.1.1',
+    version='0.1.2',
     description='Package for grid cell analysis and simulation.',
     author='Lukas Solanka',
     author_email='lsolanka@gmail.com',
